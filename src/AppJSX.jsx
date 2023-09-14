f49e47a3f25fbd8ce80f352d2b8282d8b1a03dcf
JUESOP <sinangun@thirautech.com>
2023-09-14 17:52:06 +0900

    component

---
+++ src/AppJSX.jsx
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function AppJSX() {
+  const name = 'esop';
+  const list = ['우유', '딸기', '바나나'];
+  return (
+    <>
+      <h1 className='orange'>{`Hello! ${name}`}</h1>
+      <h1>Hello!</h1>
+      <ul>
+        {
+          list.map(item => <li>{item}</li>)
+        }
+        
+      </ul>
+    </>
+  );
+}
+
+export default AppJSX;
